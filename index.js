8eb850c33daf11b446d09c49fa6bd5b577173a0f
Doreen <doreenlulu97@gmail.com>
2022-08-06 23:32:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let messages = [];
+    let i =0;
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+return messages;
+}
+console.log(writeCards(["Guadalupe","Ollie","Aki"], "surprise"));
+
+function countDown() {
+    let countDown = 0;
+    while (countDown <11) {
+        console.log(countDown++);
+    }
+}
